ea7d25a84c5cf55cdf10fc689bc3731090c1d0d2
Kamal12073 <70611780+Kamal12073@users.noreply.github.com>
2020-11-22 01:16:37 -0330

    updating demo.R

--- demo.R
+++ demo.R
@@ -9,9 +9,13 @@ Options:
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
 " -> doc
-
 library(docopt)
 opt <- docopt(doc)
-print(opt)
-print(typeof(opt))
-print(opt$arg4)
+
+main <- function(arg){
+    print(arg)
+    print(typeof(arg))
+    print(arg$arg4)
+}
+
+main(opt)
